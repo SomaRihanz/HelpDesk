ecc2e02e6cf1d0b74e8944fcceddd8db4488f8d0
SomaRihanz <naramirezg@itc.edu.co>
2023-06-01 20:43:50 -0500

    cambio en delete y css

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -5,6 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
+actualizarRegistro(result: any) {
+  throw new Error('Method not implemented.');
+}
 
 constructor(public http:HttpClient){}
 url= "https://localhost:7115/api/"
